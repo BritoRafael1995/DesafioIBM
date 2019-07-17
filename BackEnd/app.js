01cafa64b61a11052ad4321c4241215fe4a5cb84
Rafael Brito <britorafael1995@gmail.com>
2019-07-17 02:23:00 -0300

    implementado app config

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,30 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var app = require('./config/app_config');
+var miojo = require('./miojo/miojoRouter');
 
-var app = express();
+app.get('/', function(req, res){
+  res.end('Bem vindo a API')
+});
 
-app.use(bodyParser.json());
-
-app.get('/maps', function(req, res) {
-    var dados = [
-      {
-        lat: -25.470991, 
-        lon: -49.271036
-      },
-      {
-        lat: -0.935586,
-        lon: -49.635540
-      },
-      {
-        lat: -2.485874, 
-        lon: -43.128493
-      }
-    ];
-  
-    res.send(JSON.stringify(dados));
-  });
-
-  
-app.listen(8000, function() {
-    console.log('Servidor rodando na porta 8000.');
-  });
+app.use(app.router);
+routes.initialize(app);
