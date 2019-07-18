6537ec879c1a5ba6cb81609155ed3e5fe3fd8026
Rafael Brito <britorafael1995@gmail.com>
2019-07-17 23:48:51 -0300

    tratando e retornando valores invalidos

--- BackEnd/miojo/miojoRouter.js
+++ BackEnd/miojo/miojoRouter.js
@@ -3,13 +3,21 @@ var router = express.Router();
 var miojoController = require('./miojoController');
 
 router.post('/', function(req, res){
-    
+
     var tempo   = req.body.tempo;
     var amp1    = req.body.amp1;
     var amp2    = req.body.amp2;
+
+
+    if(amp1 <= tempo || amp2 <= tempo)
+        return res.status(500).end("O tempo das ampulhetas precisam ser maiores que o tempo de cozimento do miojo");
+
+    if((tempo % 2 != 0 && (amp1 % 2 == 0 && amp2 % 2 == 0)) || amp1 == amp2)
+        return res.status(500).end("Não é possivel preparar o miojo no tempo correto com estas ampulhetas");
+      
   
     miojoController.tempoPreparo(tempo, amp1, amp2, function(resp){
-        res.json(resp);
+        return res.json(resp);
     })
 });
 
