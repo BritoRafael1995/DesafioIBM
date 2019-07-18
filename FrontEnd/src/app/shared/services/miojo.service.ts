64f6ad54f6c5c24f51bb12d8ed0cc96be41231c0
Rafael Brito <britorafael1995@gmail.com>
2019-07-17 23:50:17 -0300

    criada chamada a api

--- FrontEnd/src/app/shared/services/miojo.service.ts
+++ FrontEnd/src/app/shared/services/miojo.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class MiojoService {
 
-  constructor() { }
+  baseUrl: string;
+  constructor(private httpClient: HttpClient ) { 
+    this.baseUrl = environment.baseUrl;
+  }
+
+  calcularTempoMiojo(parametros){
+    return this.httpClient.post(this.baseUrl, parametros);
+  }
 }
