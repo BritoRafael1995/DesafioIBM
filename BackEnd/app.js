17f493d026d79db2d253471f80594eaa1fb022ee
Rafael Brito <britorafael1995@gmail.com>
2019-07-17 21:44:46 -0300

    chamando rota miojo

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,9 +1,4 @@
 var app = require('./config/app_config');
 var miojo = require('./miojo/miojoRouter');
 
-app.get('/', function(req, res){
-  res.end('Bem vindo a API')
-});
-
-app.use(app.router);
-routes.initialize(app);
+app.use('/api', miojo);
